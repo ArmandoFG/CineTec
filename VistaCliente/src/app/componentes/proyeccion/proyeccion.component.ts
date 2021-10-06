32f072126ae5f2e1d998838d91543911049ef94b
FALCH2000 <faleiva.ch@gmai.com>
2021-10-06 08:34:08 -0600

    Sección de asientos

--- VistaCliente/src/app/componentes/proyeccion/proyeccion.component.ts
+++ VistaCliente/src/app/componentes/proyeccion/proyeccion.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./proyeccion.component.css']
 })
 export class ProyeccionComponent implements OnInit {
+  proyecciones: any[] = [];
+  public nombre_pelicula:any="";
 
-  constructor() { }
+  constructor() {
+    
+   }
 
   ngOnInit(): void {
+    this.proyecciones= [
+      {"hora": "8:00"},
+      {"hora": "13:00"},
+      {"hora": "23:00"}
+    ]
+    this.nombre_pelicula="Coco";
   }
-
+  
 }
